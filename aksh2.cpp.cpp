b05d3f1e0c506bbf16ba22eaf8dc026a9716064c
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-08-22 07:25:46 +0000

    Add files via upload

---
+++ aksh2.cpp.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+float fahrenhiet, celsius;
+cout << "convert fahrenhiet into celsius\n";
+cin >> fahrenhiet;
+celsius = (fahrenhiet-32)/1.8;
+cout <<"the temperature in celsius is - "<< celsius << endl;
+return 0;
+}
